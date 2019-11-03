c9552aa3a7b32b15593574cb312c105c03215798
LORDBABUINO <jorge.x7@gmail.com>
2019-11-03 09:12:31 -0300

    Imports react page on router

--- src/commands/generate:page.js
+++ src/commands/generate:page.js
@@ -1,14 +1,18 @@
 module.exports = {
   name: 'generate:page',
   description: 'Create new component inside src/pages',
-  run: async (toolbox) => {
+  run: async toolbox => {
     const {
       parameters,
       createComponent,
+      system: { run }
     } = toolbox
 
     const name = parameters.first
 
-    await createComponent('src/pages', name)
-  },
+    createComponent('src/pages', name)
+    run(
+      `sed -i 's/\nconst Routes/import ${name} from \\'/pages/${name}\\'/g' INPUTFILE`
+    )
+  }
 }
