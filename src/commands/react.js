322470994ea29db74dc83dcfde31bfbe1cb51d8e
LORDBABUINO <jorge.x7@gmail.com>
2019-11-03 09:13:43 -0300

    refactoring

--- src/commands/react.js
+++ src/commands/react.js
@@ -3,22 +3,24 @@ const inquirer = require('inquirer')
 module.exports = {
   name: 'react',
   description: 'Create new React project',
-  run: async (toolbox) => {
+  run: async toolbox => {
     const {
       parameters,
       template,
       createComponent,
       system: { run },
-      print: { info, success },
+      print: { info, success }
     } = toolbox
 
     const askName = {
       type: 'input',
       name: 'name',
-      message: 'What will be the name of your project?',
+      message: 'What will be the name of your project?'
     }
 
-    const name = parameters.first ? parameters.first : (await inquirer.prompt([askName])).name
+    const name = parameters.first
+      ? parameters.first
+      : (await inquirer.prompt([askName])).name
 
     info(`Creating new React SPA: ${name}`)
 
@@ -26,7 +28,7 @@ module.exports = {
       await template.generate({
         template: 'react/package.json.ejs',
         target: `${name}/package.json`,
-        props: { name },
+        props: { name }
       })
       info('Installing dependencies')
       await run(`yarn --cwd ${name} && yarn upgrade -L --cwd ${name}`)
@@ -36,46 +38,48 @@ module.exports = {
     await Promise.all([
       template.generate({
         template: 'react/App.js',
-        target: `${name}/src/App.js`,
+        target: `${name}/src/App.js`
       }),
       template.generate({
         template: 'react/global.js',
-        target: `${name}/src/styles/global.js`,
+        target: `${name}/src/styles/global.js`
       }),
       template.generate({
         template: 'react/index.js',
-        target: `${name}/src/index.js`,
+        target: `${name}/src/index.js`
       }),
       template.generate({
         template: 'react/routes.js',
-        target: `${name}/src/routes.js`,
+        target: `${name}/src/routes.js`
       }),
       template.generate({
         template: 'react/index.html.ejs',
-        target: `${name}/src/public/index.html`,
-        props: { name },
+        target: `${name}/public/index.html`,
+        props: { name }
       }),
       template.generate({
         template: 'react/editorconfig',
-        target: `${name}/.editorconfig`,
+        target: `${name}/.editorconfig`
       }),
       template.generate({
         template: 'react/eslintrc.js',
-        target: `${name}/.eslintrc.js`,
+        target: `${name}/.eslintrc.js`
       }),
       template.generate({
         template: 'react/prettierrc',
-        target: `${name}/.prettierrc`,
+        target: `${name}/.prettierrc`
       }),
       template.generate({
         template: 'react/gitignore',
-        target: `${name}/.gitignore`,
+        target: `${name}/.gitignore`
       }),
       installDependencies(),
-      createComponent(`${name}/src/pages`, 'Main', name),
+      createComponent(`${name}/src/pages`, 'Home', name)
     ])
 
     info('Making first commit')
-    run(`git --git-dir ${name}/.git --work-tree ${name} init && git --git-dir ${name}/.git --work-tree ${name} add . && git --git-dir ${name}/.git --work-tree ${name} commit -m 'Project initialized'`)
-  },
+    run(
+      `git --git-dir ${name}/.git --work-tree ${name} init && git --git-dir ${name}/.git --work-tree ${name} add . && git --git-dir ${name}/.git --work-tree ${name} commit -m 'Project initialized'`
+    )
+  }
 }
