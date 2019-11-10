7ac97786d6c8b16686a1cd41314593075491175e
LORDBABUINO <jorge.x7@gmail.com>
2019-11-10 09:07:53 -0300

    Fix route

--- src/templates/react/routes.js
+++ src/templates/react/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Main from './pages/Main'
+import Home from './pages/Home'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Main} />
+      <Route path="/" exact component={Home} />
     </Switch>
   </BrowserRouter>
 )
