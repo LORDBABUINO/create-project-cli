a6f2f2814c4eaf6856b561ad2be19a9349334d5c
LORDBABUINO <jorge.x7@gmail.com>
2019-11-10 08:48:12 -0300

    Adds react hooks rules on eslint

--- src/templates/react/eslintrc.js
+++ src/templates/react/eslintrc.js
@@ -1,37 +1,32 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-    'prettier',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      { extensions: ['.jsx', '.js'] },
-    ],
-    'react/state-in-constructor': ['warn', 'never'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
+    'react/state-in-constructor': ['warn', 'never'],
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
     semi: ['error', 'never'],
-  },
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 }
