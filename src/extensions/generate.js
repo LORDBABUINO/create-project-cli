4d2223c78bec1a85b516da969432f0d9738e5223
LORDBABUINO <jorge.x7@gmail.com>
2020-01-18 01:54:49 -0300

    Implements functional programming partialy

--- src/extensions/generate.js
+++ src/extensions/generate.js
@@ -3,64 +3,53 @@ export default toolbox => {
     template,
     filesystem: aFilesystem,
     parameters: { options },
-    patching: { patch },
-    print: { success, error }
+    patch
   } = toolbox
 
   const filesystem = options.dir ? aFilesystem.dir(options.dir) : aFilesystem
 
-  function isReactNative(root) {
-    const packageJson = filesystem.read(`${root || '.'}/package.json`, 'json')
-    if (!packageJson)
-      throw new Error(
-        `${root || '.'}/package.json not found\ncwd: ${filesystem.cwd()}`
-      )
-    return !!packageJson.dependencies['react-native']
-  }
+  const isReactNative = root =>
+    !!filesystem.read(`${root || '.'}/package.json`, 'json').dependencies[
+      'react-native'
+    ]
 
-  async function component(folder, name, root) {
-    if (!name) {
-      error('Component name must be specified')
-      return
-    }
-    const styleTemplate = (await isReactNative(root))
-      ? 'styles-rn.js.ejs'
-      : 'styles-react.js.ejs'
-    await Promise.all([
+  const component = (folder, name, root) =>
+    Promise.all([
       template.generate({
         template: 'component.js.ejs',
         target: `${folder}/${name}/index.js`,
         props: { name }
       }),
       template.generate({
-        template: styleTemplate,
+        template: isReactNative(root)
+          ? 'styles-rn.js.ejs'
+          : 'styles-react.js.ejs',
         target: `${folder}/${name}/styles.js`
       })
     ])
-    success(`Generated ${folder}/${name}.`)
-  }
 
-  async function page(name, root) {
-    const pathRoutes = `${options.dir || '.'}/src/routes.js`
+  const insetNavigator = (root, name) =>
+    isReactNative(root || '.')
+      ? {
+          insert: `${name},`,
+          after: /Navigator\s*{\s*/
+        }
+      : {
+          insert: `<Route path="/${name}" component={${name}} />\n`,
+          after: /<Switch>\s*/
+        }
 
-    component(`${options.dir || '.'}/src/pages`, name, root)
-
-    await patch(pathRoutes, {
-      insert: `import ${name} from './pages/${name}'\n`,
-      before: '\nconst Routes'
-    })
-    patch(
-      pathRoutes,
-      isReactNative(root)
-        ? {
-            insert: `\n      ${name},`,
-            after: 'Navigator(\n    {'
-          }
-        : {
-            insert: `\n      <Route path="/${name}" component={${name}} />`,
-            before: '\n    <Switch>'
-          }
-    )
-  }
+  const page = (name, root) =>
+    Promise.all([
+      component(`${root || '.'}/src/pages`, name, root),
+      patch(
+        `${root || '.'}/src/routes.js`,
+        {
+          insert: `import ${name} from './pages/${name}'\n`,
+          before: '\nconst Routes'
+        },
+        insetNavigator(root || '.', name)
+      )
+    ])
   toolbox.generate = { component, page, isReactNative }
 }
