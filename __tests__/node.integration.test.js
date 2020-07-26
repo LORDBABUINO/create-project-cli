54f4ab1e96f3de8889aed54871d7b5131ab5c16f
LORDBABUINO <jorge.x7@gmail.com>
2020-07-26 01:41:42 -0300

    Inits file structure

--- __tests__/node.integration.test.js
+++ __tests__/node.integration.test.js
@@ -18,9 +18,63 @@ describe('node command integration', () => {
 
   afterEach(async () => system.run(`rm -rf ${project}`))
 
-  it('should install express', () => {
-    const dir = filesystem.path(projectPath, 'node_modules', 'express')
+  it('should install default dependencies', async () => {
+    const dependencies = [
+      ['bcryptjs'],
+      ['date-fns'],
+      ['dotenv'],
+      ['express'],
+      ['jsonwebtoken'],
+      ['mongoose'],
+      ['sucrase'],
+      ['@sucrase', 'jest-plugin'],
+      ['eslint'],
+      ['eslint-config-airbnb-base'],
+      ['eslint-config-prettier'],
+      ['eslint-plugin-import'],
+      ['eslint-plugin-jest'],
+      ['eslint-plugin-prettier'],
+      ['jest'],
+      ['nodemon'],
+      ['prettier'],
+      ['supertest'],
+    ]
 
-    expect(filesystem.exists(dir)).toBeTruthy()
+    const modulesPath = filesystem.path(projectPath, 'node_modules')
+    const tree = await system.run(`tree ${modulesPath} -inafd --noreport -L 2`)
+    return Promise.all(
+      dependencies.map(async (dependencie) => {
+        expect(tree).toContain(filesystem.path(modulesPath, ...dependencie))
+      })
+    )
+  })
+
+  it('should add default templates', async () => {
+    const files = [
+      ['.eslintrc.js'],
+      ['.editorconfig'],
+      ['.gitignore'],
+      ['nodemon.json'],
+      ['.env.example'],
+      ['.prettierrc'],
+      ['jest.config.js'],
+      ['docker-compose.yml'],
+      ['src', 'app.js'],
+      ['src', 'server.js'],
+      ['src', 'routes.js'],
+      ['src', 'database', 'index.js'],
+      ['src', 'config', 'auth.js'],
+      ['src', 'app', 'middlewares', 'auth.js'],
+      ['src', 'app', 'controllers', 'SessionController.js'],
+    ]
+
+    const tree = await system.run(
+      `tree ${projectPath} -I 'node_modules|coverage|.git' -inaf --noreport`
+    )
+    return Promise.all(
+      files.map(async (file) => {
+        expect(tree).toContain(filesystem.path(projectPath, ...file))
+      })
+    )
   })
 })
--- src/commands/node.js
+++ src/commands/node.js
@@ -5,6 +5,66 @@ module.exports = {
     buildMainFunction('Project initialized', [
       [
         {
+          template: 'node/.editorconfig',
+          target: '.editorconfig',
+        },
+        {
+          template: 'node/.env.example',
+          target: '.env.example',
+        },
+        {
+          template: 'node/.eslintrc.js',
+          target: '.eslintrc.js',
+        },
+        {
+          template: 'node/.gitignore',
+          target: '.gitignore',
+        },
+        {
+          template: 'node/.prettierrc',
+          target: '.prettierrc',
+        },
+        {
+          template: 'node/app.js',
+          target: 'src/app.js',
+        },
+        {
+          template: 'node/auth.js',
+          target: 'src/config/auth.js',
+        },
+        {
+          template: 'node/docker-compose.yml',
+          target: 'docker-compose.yml',
+        },
+        {
+          template: 'node/index.js',
+          target: 'src/database/index.js',
+        },
+        {
+          template: 'node/jest.config.js',
+          target: 'jest.config.js',
+        },
+        {
+          template: 'node/middleware-auth.js',
+          target: 'src/app/middlewares/auth.js',
+        },
+        {
+          template: 'node/nodemon.json',
+          target: 'nodemon.json',
+        },
+        {
+          template: 'node/routes.js',
+          target: 'src/routes.js',
+        },
+        {
+          template: 'node/server.js',
+          target: 'src/server.js',
+        },
+        {
+          template: 'node/SessionController.js',
+          target: 'src/app/controllers/SessionController.js',
+        },
+        {
           install: {
             packages: [
               'bcryptjs',
